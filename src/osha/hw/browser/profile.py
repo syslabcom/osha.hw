8a83ddf1eda0e37075e055d08fe948c6899a0eb4
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-05 14:16:05 +0000

    added fop detail, made profile.py simpler

--- src/osha/hw/browser/profile.py
+++ src/osha/hw/browser/profile.py
@@ -4,14 +4,14 @@ from osha.hw.browser.sync_receiver import BaseDBView
 from osha.hw.queries import *
 
 
-class OCPsOverview(BaseDBView):
+class _OrgOverview(BaseDBView):
 
     def get_partners(self):
-        res = self.conn.execute(get_ocps)
+        res = self.conn.execute(self.get_org_q)
         return res
 
     def get_partners_atoz(self):
-        partners = self.conn.execute(get_ocps)
+        partners = self.conn.execute(self.get_org_q)
         atoz = dict()
         for partner in partners:
             initial = partner["title"][0].upper()
@@ -19,41 +19,45 @@ class OCPsOverview(BaseDBView):
                 atoz[initial].append(partner)
             else:
                 atoz[initial] = [partner]
-
         return atoz
 
 
-class FOPsOverview(BaseDBView):
+class OCPsOverview(_OrgOverview):
 
-    def get_partners(self):
-        res = self.conn.execute(get_fops)
-        return res
-
-    def get_partners_atoz(self):
-        partners = self.conn.execute(get_fops)
-        atoz = dict()
-        for partner in partners:
-            initial = partner["title"][0].upper()
-            if initial in atoz:
-                atoz[initial].append(partner)
-            else:
-                atoz[initial] = [partner]
-
-        return atoz
+    get_org_q = get_ocps
 
 
-class OCPDetail(BaseDBView):
+class FOPsOverview(_OrgOverview):
+
+    get_org_q = get_fops
+
+
+class _OrgDetail(BaseDBView):
 
     def get_partner(self, id=''):
-        partner = self.conn.execute(get_ocp_by_id % dict(id=id))
+        partner = self.conn.execute(self.get_org_by_id_q % dict(id=id))
         return partner.fetchone()
 
     def get_partner_events(self, partner_id=''):
-        events = self.conn.execute(get_ocp_events % \
+        events = self.conn.execute(self.get_org_events_q % \
         dict(partner_id=partner_id))
         return events
 
     def get_partner_news(self, partner_id=''):
-        events = self.conn.execute(get_ocp_news % \
+        events = self.conn.execute(self.get_org_news_q % \
         dict(partner_id=partner_id))
         return events
+
+
+class OCPDetail(_OrgDetail):
+
+    get_org_by_id_q = get_ocp_by_id
+    get_org_events_q = get_ocp_events
+    get_org_news_q = get_ocp_news
+
+
+class FOPDetail(_OrgDetail):
+
+    get_org_by_id_q = get_fop_by_id
+    get_org_events_q = get_fop_events
+    get_org_news_q = get_fop_news
