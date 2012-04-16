511a73cea9cade92c2dea44e9597413ed8b4b9d3
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-16 14:03:59 +0000

    small helper change

--- src/osha/hw/browser/helper.py
+++ src/osha/hw/browser/helper.py
@@ -398,9 +398,9 @@ class HelperView(BrowserView):
 
     def cls_by_str(self, text):
         """ calculate text size indicator for styling. Used in the navigation """
-        if len(text)<20:
+        if len(text)<=20:
             return None
-        elif 20<=len(text)<50:
+        elif 20<len(text)<50:
             return 'char-20'
         else:
             return 'char-50'
