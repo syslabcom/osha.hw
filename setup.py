62adc2a02337c1c9f7c41c7b497fe7288eb3d138
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-19 16:53:53 +0000

    Preparing release 0.4.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4.6dev'
+version = '0.4.6'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
