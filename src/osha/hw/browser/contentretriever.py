e8d8762ee91718ee77bfe697827eefa01c8200cb
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-20 14:01:41 +0000

    Fixing the news teaser box, so that the current ball gets highlighted

--- src/osha/hw/browser/contentretriever.py
+++ src/osha/hw/browser/contentretriever.py
@@ -10,22 +10,24 @@ from Products.Five import BrowserView
 from Products.Archetypes import PloneMessageFactory as _
 from ZPublisher import HTTPResponse
 
+
 class NewsByPathView(BrowserView):
-    """ When called with a path, it renders a news so that it fits into the homepage box """
+    """ When called with a counter, it renders a news so that it fits into 
+    the homepage box """
     index = ViewPageTemplateFile("templates/news_by_path.pt")
     
     def __call__(self):
         no = int(self.request.get('no', 1))
         # careful, no is repeat/news/number, so 1..n, not 0..n-1
         helper = self.context.restrictedTraverse('@@hw_view')
-        news = [x for x in helper.getNews(limit=3, strip_links=1)]
-        if 0<no<4:
-            self.news = news[no-1]
+        self.news = [x for x in helper.getNews(limit=3, strip_links=1)]
+        if 0 < no < 4:
+            self.current_news = self.news[no-1]
         else:
-            self.news = {}
+            self.current_news = {}
         return self.index(self)
-            
-            
+
+
 class EventByPathView(BrowserView):
     """ When called with a path, it renders a news so that it fits into the homepage box """
     index = ViewPageTemplateFile("templates/event_by_path.pt")
