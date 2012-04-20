9b46ececeeecdae5fe7ffaf0b78164b2d2b50b47
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-20 15:59:26 +0000

    pep8

--- src/osha/hw/browser/contentretriever.py
+++ src/osha/hw/browser/contentretriever.py
@@ -12,10 +12,10 @@ from ZPublisher import HTTPResponse
 
 
 class NewsByPathView(BrowserView):
-    """ When called with a counter, it renders a news so that it fits into 
+    """ When called with a counter, it renders a news so that it fits into
     the homepage box """
     index = ViewPageTemplateFile("templates/news_by_path.pt")
-    
+
     def __call__(self):
         no = int(self.request.get('no', 1))
         # careful, no is repeat/news/number, so 1..n, not 0..n-1
@@ -23,25 +23,25 @@ class NewsByPathView(BrowserView):
         self.news = [x for x in helper.getNews(limit=3, strip_links=1)]
         self.newsfolder_url = helper.getNewsfolderUrl()
         if 0 < no < 4:
-            self.current_news = self.news[no-1]
+            self.current_news = self.news[no - 1]
         else:
             self.current_news = {}
         return self.index(self)
 
 
 class EventByPathView(BrowserView):
-    """ When called with a path, it renders a news so that it fits into the homepage box """
+    """ When called with a counter, it renders a news so that it fits into
+    the homepage box """
     index = ViewPageTemplateFile("templates/event_by_path.pt")
-    
+
     def __call__(self):
         no = int(self.request.get('no', 1))
         # careful, no is repeat/news/number, so 1..n, not 0..n-1
         helper = self.context.restrictedTraverse('@@hw_view')
         self.events = [x for x in helper.getEvents(limit=3)]
         self.eventsfolder_url = helper.getEventsfolderUrl()
-        if 0<no<4:
-            self.current_event = self.events[no-1]
+        if 0 < no < 4:
+            self.current_event = self.events[no - 1]
         else:
             self.current_event = {}
         return self.index(self)
-            
